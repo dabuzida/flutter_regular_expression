32477b9e80bb20685de761ce7a682a3da74ff862
dabuzida <dabuzida@gmail.com>
2022-08-08 15:14:09 +0900

    1

---
+++ lib/regular_expression.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class RegularExpression extends StatefulWidget {
+  const RegularExpression({Key? key}) : super(key: key);
+
+  @override
+  State<RegularExpression> createState() => _RegularExpressionState();
+}
+
+class _RegularExpressionState extends State<RegularExpression> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('d'));
+  }
+}
